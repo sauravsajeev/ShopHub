52d11fd29933a5cd3c94d58a620425bcc73eec98
Juzznick <sauravsajeev47@gmail.com>
2025-09-06 17:32:50 +0530

    Second commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ import cartRoutes from "./routes/cart.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://wshophub.netlify.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
